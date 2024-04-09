a2bba801283ce0027d210840d372607ed3219ba8
Mỹ Nguyên <77737848+MNguyen1302@users.noreply.github.com>
2024-04-10 00:18:08 +0700

    thành viên

--- Member-Management/src/main/java/UI/ThietBi/themThietBiForm.java
+++ Member-Management/src/main/java/UI/ThietBi/themThietBiForm.java
@@ -1,19 +1,17 @@
 package UI.ThietBi;
 
-import BLL.thietbiBLL;
+import BLL.ThietBiBLL;
 import DAL.ThietBi.thietbi;
-import DAL.ThietBi.thietbiDAL;
+import DAL.ThietBi.ThietBiDAL;
 import javax.swing.JOptionPane;
 
 public class themThietBiForm extends javax.swing.JFrame {
 
-    private thietbiBLL tbBLL = new thietbiBLL();
-    private thietbiDAL tbDAL = new thietbiDAL();
-    private thietbiPanel tbPanel;
+    private ThietBiBLL tbBLL = new ThietBiBLL();
+    private ThietBiDAL tbDAL = new ThietBiDAL();
 
     public themThietBiForm() {
         initComponents();
-
     }
 
     @SuppressWarnings("unchecked")
@@ -21,10 +19,10 @@ public class themThietBiForm extends javax.swing.JFrame {
     private void initComponents() {
 
         txtName = new javax.swing.JLabel();
-        txtTentb = new javax.swing.JTextField();
+        jtTentb = new javax.swing.JTextField();
         txtDes = new javax.swing.JLabel();
-        txtMota = new javax.swing.JTextField();
-        txtMatb = new javax.swing.JTextField();
+        jtMota = new javax.swing.JTextField();
+        jtMatb = new javax.swing.JTextField();
         txtID = new javax.swing.JLabel();
         btnReset = new javax.swing.JButton();
         btnThem = new javax.swing.JButton();
@@ -66,15 +64,15 @@ public class themThietBiForm extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(txtID)
                                 .addGap(57, 57, 57)
-                                .addComponent(txtMatb, javax.swing.GroupLayout.PREFERRED_SIZE, 272, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(jtMatb, javax.swing.GroupLayout.PREFERRED_SIZE, 272, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addGroup(layout.createSequentialGroup()
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(txtName)
                                     .addComponent(txtDes))
                                 .addGap(43, 43, 43)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(txtTentb)
-                                    .addComponent(txtMota)))))
+                                    .addComponent(jtTentb)
+                                    .addComponent(jtMota)))))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(96, 96, 96)
                         .addComponent(btnReset)
@@ -89,16 +87,16 @@ public class themThietBiForm extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(62, 62, 62)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtMatb, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtMatb, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtID))
                 .addGap(62, 62, 62)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtName)
-                    .addComponent(txtTentb, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jtTentb, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(62, 62, 62)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(txtDes)
-                    .addComponent(txtMota, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jtMota, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(31, 31, 31)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(btnReset)
@@ -111,22 +109,27 @@ public class themThietBiForm extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnThemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnThemActionPerformed
-        tbPanel = new thietbiPanel();
+        thietbiPanel tbPanel = new thietbiPanel();
         // Lấy thông tin từ các trường nhập liệu
-        String strMaTB = txtMatb.getText().trim();
-        String tenTB = txtTentb.getText();
-        String moTaTB = txtMota.getText();
+        int maTB = Integer.parseInt(jtMatb.getText());
+        String tenTB = jtTentb.getText();
+        String moTaTB = jtMota.getText();
 
         // Kiểm tra xem các trường dữ liệu có rỗng không
-        if (strMaTB.isEmpty() || tenTB.isEmpty()) {
+        if (jtMatb.getText().trim().isEmpty() || tenTB.isEmpty() || moTaTB.isEmpty()) {
             // Hiển thị hộp thoại cảnh báo yêu cầu nhập đầy đủ thông tin
-            JOptionPane.showMessageDialog(this, "Mã thiết bị hoặc tên thiết bị trống, vui lòng nhập đầy đủ thông tin", "Cảnh báo", JOptionPane.WARNING_MESSAGE);
+            JOptionPane.showMessageDialog(this, "Vui lòng nhập đầy đủ thông tin", "Cảnh báo", JOptionPane.WARNING_MESSAGE);
             return; // Không thực hiện thêm thiết bị nếu có trường dữ liệu rỗng
-        }
+        } else {
+            // Lấy giá trị của trường MaTB
+            try {
+                maTB = Integer.parseInt(jtMatb.getText().trim());
+            } catch (NumberFormatException e) {
+                // Hiển thị hộp thoại cảnh báo nếu trường MaTB không phải là số
+                JOptionPane.showMessageDialog(this, "Vui lòng nhập số vào trường Mã thiết bị", "Cảnh báo", JOptionPane.WARNING_MESSAGE);
+                return; // Không thực hiện thêm thiết bị nếu MaTB không phải là số
+            }
 
-        // Kiểm tra xem MaTB có phải là số nguyên hay không
-        try {
-            int maTB = Integer.parseInt(strMaTB);
             // Kiểm tra xem mã thiết bị đã tồn tại hay chưa
             if (tbDAL.isMaTbExisted(maTB)) {
                 // Hiển thị hộp thoại cảnh báo nếu mã thiết bị đã tồn tại
@@ -135,22 +138,26 @@ public class themThietBiForm extends javax.swing.JFrame {
             }
             // Tạo một đối tượng thietbi mới
             thietbi tbMoi = new thietbi(maTB, tenTB, moTaTB);
-            tbBLL.addThietbi(tbMoi); // Thêm thiết bị mới vào cơ sở dữ liệu
-            if (tbBLL.isSuccess()) {
-                JOptionPane.showMessageDialog(this, "Đã thêm thiết bị thành công", "Thông báo", JOptionPane.INFORMATION_MESSAGE);
-                tbPanel.loadThietbi();
-            }
-        } catch (NumberFormatException e) {
-            // Hiển thị hộp thoại cảnh báo nếu MaTB không phải là số
-            JOptionPane.showMessageDialog(this, "Mã thiết bị phải là số nguyên", "Cảnh báo", JOptionPane.WARNING_MESSAGE);
+
+            // Thêm thiết bị mới vào cơ sở dữ liệu
+            tbBLL.addThietbi(tbMoi);
+            JOptionPane.showMessageDialog(this, "Đã thêm thiết bị thành công", "Thông báo", JOptionPane.INFORMATION_MESSAGE);
+
+            // Set visibility of current frame to false
+            setVisible(false);
+            tbPanel.setVisible(true);
+            tbPanel.loadThietbi();
+
         }
     }//GEN-LAST:event_btnThemActionPerformed
 
     private void btnbackActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnbackActionPerformed
-        tbPanel = new thietbiPanel();
+        // Set visibility of current frame to false
         setVisible(false);
+
+        // Show thietbiPanel
+        thietbiPanel tbPanel = new thietbiPanel();
         tbPanel.setVisible(true);
-        tbPanel.loadThietbi();
 
     }//GEN-LAST:event_btnbackActionPerformed
 
@@ -166,11 +173,11 @@ public class themThietBiForm extends javax.swing.JFrame {
     private javax.swing.JButton btnReset;
     private javax.swing.JButton btnThem;
     private javax.swing.JButton btnback;
+    private javax.swing.JTextField jtMatb;
+    private javax.swing.JTextField jtMota;
+    private javax.swing.JTextField jtTentb;
     private javax.swing.JLabel txtDes;
     private javax.swing.JLabel txtID;
-    private javax.swing.JTextField txtMatb;
-    private javax.swing.JTextField txtMota;
     private javax.swing.JLabel txtName;
-    private javax.swing.JTextField txtTentb;
     // End of variables declaration//GEN-END:variables
 }
--- Member-Management/src/main/java/UI/ThietBi/thietbiPanel.java
+++ Member-Management/src/main/java/UI/ThietBi/thietbiPanel.java
@@ -1,9 +1,7 @@
 package UI.ThietBi;
 
-import BLL.thietbiBLL;
-import DAL.HibernateUtils;
+import BLL.ThietBiBLL;
 import DAL.ThietBi.thietbi;
-import DAL.ThietBi.thietbiDAL;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,23 +9,20 @@ import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
-import java.io.IOException;
-import java.text.ParseException;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Cell;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
+//import java.io.IOException;
+//import java.text.ParseException;
+//import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+//import org.apache.poi.ss.usermodel.Sheet;
+//import org.apache.poi.ss.usermodel.Workbook;
+//import org.apache.poi.ss.usermodel.WorkbookFactory;
+//import org.apache.poi.ss.usermodel.Row;
+//import org.apache.poi.ss.usermodel.Cell;
 
 public class thietbiPanel extends javax.swing.JPanel {
 
-    private thietbiBLL tbBLL = new thietbiBLL();
-    private themThietBiForm themtb = new themThietBiForm();
-    private thietbiDAL tbDAL = new thietbiDAL();
-    DefaultTableModel model = new DefaultTableModel();
+    private ThietBiBLL tbBLL = new ThietBiBLL();
+    DefaultTableModel model;
+    themThietBiForm themtb = new themThietBiForm();
 
     public thietbiPanel() {
         initComponents();
@@ -35,26 +30,32 @@ public class thietbiPanel extends javax.swing.JPanel {
     }
 
     public void loadThietbi() {
+        // Get the list of thietbi objects from the BLL
         List<thietbi> thietbiList = tbBLL.loadThietbi();
+
+        // Create a DefaultTableModel for the table jtQLTB
         model = (DefaultTableModel) jtQLTB.getModel();
+
+        // Clear existing rows from the table
         model.setRowCount(0);
+
+        // Iterate through the thietbiList and add each thietbi object to the table
         for (thietbi tb : thietbiList) {
             Object[] row = {tb.getMaTB(), tb.getTenTB(), tb.getMoTaTB()};
             model.addRow(row);
         }
-
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        txtMatb = new javax.swing.JTextField();
+        jtMatb = new javax.swing.JTextField();
         txtID = new javax.swing.JLabel();
         txtName = new javax.swing.JLabel();
-        txtTentb = new javax.swing.JTextField();
+        jtTentb = new javax.swing.JTextField();
         txtDes = new javax.swing.JLabel();
-        txtMota = new javax.swing.JTextField();
+        jtMota = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         jtQLTB = new javax.swing.JTable();
         jLabel4 = new javax.swing.JLabel();
@@ -93,11 +94,6 @@ public class thietbiPanel extends javax.swing.JPanel {
         jLabel4.setText("QUẢN LÝ THIẾT BỊ");
 
         btnSua.setText("Sửa");
-        btnSua.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnSuaActionPerformed(evt);
-            }
-        });
 
         btnXoa.setText("Xóa");
 
@@ -124,7 +120,7 @@ public class thietbiPanel extends javax.swing.JPanel {
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(txtID)
                                 .addGap(57, 57, 57)
-                                .addComponent(txtMatb, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addComponent(jtMatb, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE))
                             .addComponent(btnSua)
                             .addComponent(btnXoa, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addGroup(layout.createSequentialGroup()
@@ -133,8 +129,8 @@ public class thietbiPanel extends javax.swing.JPanel {
                                     .addComponent(txtDes))
                                 .addGap(43, 43, 43)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(txtTentb, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
-                                    .addComponent(txtMota)))
+                                    .addComponent(jtTentb, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
+                                    .addComponent(jtMota)))
                             .addComponent(cbThem, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(25, 25, 25)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 621, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -153,16 +149,16 @@ public class thietbiPanel extends javax.swing.JPanel {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(90, 90, 90)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(txtMatb, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jtMatb, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(txtID))
                         .addGap(40, 40, 40)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(txtName)
-                            .addComponent(txtTentb, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jtTentb, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(40, 40, 40)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(txtDes)
-                            .addComponent(txtMota, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jtMota, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(40, 40, 40)
                         .addComponent(cbThem, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -176,14 +172,13 @@ public class thietbiPanel extends javax.swing.JPanel {
     private void jtQLTBMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jtQLTBMouseClicked
         int i = jtQLTB.getSelectedRow();
         if (i >= 0) {
-            txtMatb.setText(model.getValueAt(i, 0).toString());
-            txtTentb.setText(model.getValueAt(i, 1).toString());
-            txtMota.setText(model.getValueAt(i, 2).toString());
+            jtMatb.setText(model.getValueAt(i, 0).toString());
+            jtTentb.setText(model.getValueAt(i, 1).toString());
+            jtMota.setText(model.getValueAt(i, 2).toString());
         }
     }//GEN-LAST:event_jtQLTBMouseClicked
 
     private void cbThemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbThemActionPerformed
-
         // Lấy lựa chọn từ combo box
         int luaChon = cbThem.getSelectedIndex();
         switch (luaChon) {
@@ -195,94 +190,81 @@ public class thietbiPanel extends javax.swing.JPanel {
                 themtb.setLocationRelativeTo(this);
                 break;
             case 2: // Trường hợp nhập theo file excel
-                importExcel();
+//                importExcel();
                 break;
             default:
                 break;
         }
     }//GEN-LAST:event_cbThemActionPerformed
 
-    private void btnSuaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSuaActionPerformed
+//    private void importExcel() {
+//        JFileChooser chooser = new JFileChooser();
+//        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+//        chooser.setFileFilter(new FileNameExtensionFilter("Excel files", "xls", "xlsx"));
+//        int returnVal = chooser.showOpenDialog(this);
+//        if (returnVal == JFileChooser.APPROVE_OPTION) {
+//            File file = chooser.getSelectedFile();
+//            try {
+//                List<thietbi> importedData = readDataFromExcel(file);
+//                if (importedData != null) {
+//                    for (thietbi tb : importedData) {
+//                        tbBLL.addThietbi(tb); // Thêm từng thiết bị vào cơ sở dữ liệu
+//                    }
+//                    JOptionPane.showMessageDialog(this, "Đã nhập dữ liệu từ Excel thành công", "Thông báo", JOptionPane.INFORMATION_MESSAGE);
+//                    // Sau khi thêm dữ liệu xong, cập nhật lại bảng hiển thị
+//                    loadThietbi();
+//                }
+//            } catch (IOException | InvalidFormatException | ParseException ex) {
+//                JOptionPane.showMessageDialog(this, "Lỗi khi nhập dữ liệu từ Excel: " + ex.getMessage(), "Lỗi", JOptionPane.ERROR_MESSAGE);
+//            }
+//        }
+//    }
+//
+//    private List<thietbi> readDataFromExcel(File file) throws IOException, InvalidFormatException, ParseException {
+//        List<thietbi> importedData = new ArrayList<>();
+//        Workbook workbook = WorkbookFactory.create(file);
+//        Sheet sheet = workbook.getSheetAt(0);
+//
+//        // Duyệt qua từng dòng và cột để đọc dữ liệu, bắt đầu từ dòng thứ hai
+//        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+//            Row row = sheet.getRow(i);
+//            if (row == null) {
+//                continue; // Bỏ qua nếu dòng là null
+//            }
+//
+//            Cell cellMaTB = row.getCell(0);
+//            Cell cellTenTB = row.getCell(1);
+//            Cell cellMoTaTB = row.getCell(2);
+//
+//            // Kiểm tra xem ô có phải là số không trước khi lấy giá trị
+//            int maTB;
+//            try {
+//                maTB = (int) cellMaTB.getNumericCellValue();
+//            } catch (IllegalStateException | NumberFormatException e) {
+//                // Nếu không thể chuyển đổi giá trị sang số, bỏ qua dòng này và hiển thị cảnh báo
+//                System.err.println("Dòng " + (i + 1) + ": Mã thiết bị không hợp lệ, bỏ qua dữ liệu từ dòng này.");
+//                continue;
+//            }
+//
+//            String tenTB = cellTenTB.getStringCellValue();
+//            String moTaTB = cellMoTaTB.getStringCellValue();
+//
+//            thietbi tb = new thietbi(maTB, tenTB, moTaTB);
+//            importedData.add(tb);
+//        }
+//        workbook.close();
+//        return importedData;
+//    }
 
-    }//GEN-LAST:event_btnSuaActionPerformed
+    public static void main(String[] args) {
+        thietbiPanel tbPanel = new thietbiPanel();
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                tbPanel.setVisible(true);
 
-    private void importExcel() {
-        JFileChooser chooser = new JFileChooser();
-        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-        chooser.setFileFilter(new FileNameExtensionFilter("Excel files", "xls", "xlsx"));
-        int returnVal = chooser.showOpenDialog(this);
-        if (returnVal == JFileChooser.APPROVE_OPTION) {
-            File file = chooser.getSelectedFile();
-            try {
-                List<thietbi> importedData = readDataFromExcel(file);
-                int importedCount = 0; // Số lượng dữ liệu đã được nhập thành công
-                for (thietbi tb : importedData) {
-                    tbBLL.addThietbi(tb);
-                    if (tbBLL.isSuccess()) {
-                        importedCount++;
-                    }
-                }
-                if (importedCount > 0) {
-                    JOptionPane.showMessageDialog(this, "Đã nhập thành công " + importedCount + " thiết bị từ Excel", "Thông báo", JOptionPane.INFORMATION_MESSAGE);
-                    // Sau khi thêm dữ liệu xong, cập nhật lại bảng hiển thị
-                    loadThietbi();
-                } else {
-                    JOptionPane.showMessageDialog(this, "Không có dữ liệu nào được nhập từ Excel", "Thông báo", JOptionPane.INFORMATION_MESSAGE);
-                }
-            } catch (IOException | InvalidFormatException | ParseException ex) {
-                JOptionPane.showMessageDialog(this, "Lỗi khi nhập dữ liệu từ Excel: " + ex.getMessage(), "Lỗi", JOptionPane.ERROR_MESSAGE);
             }
-        }
+        });
     }
-
-    private List<thietbi> readDataFromExcel(File file) throws IOException, InvalidFormatException, ParseException {
-        List<thietbi> importedData = new ArrayList<>();
-        Workbook workbook = WorkbookFactory.create(file);
-        Sheet sheet = workbook.getSheetAt(0);
-
-        // Duyệt qua từng dòng và cột để đọc dữ liệu, bắt đầu từ dòng thứ hai
-        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
-            Row row = sheet.getRow(i);
-            if (row == null) {
-                continue; // Bỏ qua nếu dòng là null
-            }
-
-            Cell cellMaTB = row.getCell(0);
-            Cell cellTenTB = row.getCell(1);
-            Cell cellMoTaTB = row.getCell(2);
-
-            // Kiểm tra xem ô có phải là số không trước khi lấy giá trị
-            int maTB;
-            try {
-                maTB = (int) cellMaTB.getNumericCellValue();
-            } catch (IllegalStateException | NumberFormatException e) {
-                // Nếu không thể chuyển đổi giá trị sang số, bỏ qua dòng này và hiển thị cảnh báo
-                JOptionPane.showMessageDialog(null, "Mã thiết bị không hợp lệ.", "Lỗi", JOptionPane.ERROR_MESSAGE);
-                continue;
-            }
-
-            String tenTB = cellTenTB.getStringCellValue();
-            String moTaTB = cellMoTaTB.getStringCellValue();
-
-            // Kiểm tra nếu mã thiết bị hoặc tên thiết bị trống
-            if (maTB == 0 || tenTB.isEmpty()) {
-                JOptionPane.showMessageDialog(null, "Mã thiết bị hoặc tên thiết bị trống, vui lòng nhập dữ liệu cho dòng này.", "Lỗi", JOptionPane.ERROR_MESSAGE);
-                continue;
-            }
-
-            // Kiểm tra nếu mã thiết bị đã tồn tại trong cơ sở dữ liệu
-            if (tbDAL.isMaTbExisted(maTB)) {
-                JOptionPane.showMessageDialog(null, "Mã thiết bị đã tồn tại trong cơ sở dữ liệu.", "Lỗi", JOptionPane.ERROR_MESSAGE);
-                continue;
-            }
-
-            thietbi tb = new thietbi(maTB, tenTB, moTaTB);
-            importedData.add(tb);
-        }
-        workbook.close();
-        return importedData;
-    }
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnSua;
@@ -290,13 +272,13 @@ public class thietbiPanel extends javax.swing.JPanel {
     private javax.swing.JComboBox<String> cbThem;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jtMatb;
+    private javax.swing.JTextField jtMota;
     private javax.swing.JTable jtQLTB;
+    private javax.swing.JTextField jtTentb;
     private javax.swing.JLabel txtDes;
     private javax.swing.JLabel txtID;
-    private javax.swing.JTextField txtMatb;
-    private javax.swing.JTextField txtMota;
     private javax.swing.JLabel txtName;
-    private javax.swing.JTextField txtTentb;
     // End of variables declaration//GEN-END:variables
 
 }
